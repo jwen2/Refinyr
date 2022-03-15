d77ce0e5704ce39c3916020c40632f2693ab395f
he-eric <eriche864@gmail.com>
2022-03-15 19:07:00 -0400

    Setup environment with docker compose file

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,21 @@
 from flask import Flask
 import pandas as pd
-# import docker
 
-class API:
-    # client = null
+app = Flask(__name__)
 
-    # def connectToClient():
-        # self.client = docker.DockerClient(base_url='')
+@app.route('/api/load')
+def load():
+    return 'hello'
 
-    @app.route('/api/upload', method=["POST"])
-    def upload(csvFile):
-        volume = client.volumes.get('{volume_id_here}')
+def isNullAny(df):
+    return df.isnull().any()
 
-    @app.route('/api/load', method=["GET"])
-    def load(csvFile):
-        df = pd.read_csv(csvFile)
-        return df
+def isNullColumn(columnName, df):
+    return df[columnName].isnull()
 
-    def isNullAny(df):
-        return df.isnull().any()
+def removeNullsFromDF(df):
+    return df.dropna()
 
-    def isNullColumn(columnName, df):
-        return df[columnName].isnull()
-
-    def removeNullsFromDF(df):
-        return df.dropna()
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0')         
 
