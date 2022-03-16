2b80f546cce972dcd83238d2aaf12843988fb4f5
he-eric <eriche864@gmail.com>
2022-03-16 19:48:53 -0400

    finish downloader and uploader. fix testcases. update readme

--- README.txt
+++ README.txt
@@ -9,9 +9,23 @@ To test the api run:
 Port is mapped to 8000 (configurable in docker compose file)
 
 To test file upload:
-    cd into testcases folder:
-        curl -F 'file=@a.csv' http://127.0.0.1:5000/uploader
+    cd into testcases/upload folder:
+        curl -F 'file=@a.csv' http://127.0.0.1:8000/uploader
 
-Cool guide to start out with for handling file uploads with flask:
+To test file download:
+    cd into testcases/download folder:
+        curl http://127.0.0.1:8000/downloader/a.csv  --output yourFileName.csv
+
+Cool guide(s) to start out with for handling file uploads with flask:
 
     https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
+    https://pythonise.com/series/learning-flask/sending-files-with-flask
+
+Refer to flask documentation:
+
+    https://flask.palletsprojects.com/en/2.0.x/api/#flask.send_from_directory
+
+To shell into container for debugging
+
+    docker exec -it {container_id} sh 
+    retrieve the container_id from: docker ps 
--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, abort, send_from_directory
 import pandas as pd
+import os
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.csv']
+app.config['UPLOAD_PATH'] = '../csv'
 
 @app.route('/api/load')
 def load():
@@ -10,11 +14,24 @@ def load():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        # f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        uploaded_file = request.files['file']
+        filename = uploaded_file.filename
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            return "saved"
     else:
         return 'get request'
+
+@app.route("/downloader/<file_name>")
+def get_image(file_name):
+    try:
+        return send_from_directory(directory=app.config["UPLOAD_PATH"], path=file_name)
+    except FileNotFoundError:
+        abort(404)        
+        
 
 def isNullAny(df):
     return df.isnull().any()
