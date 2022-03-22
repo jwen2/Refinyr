739717e81f571844751f527f81ff0536bb5a70b0
he-eric <eriche864@gmail.com>
2022-03-22 16:28:43 -0400

    fix issues with insertions (defined constant and method with same name...), also don't use  as column name...

--- backend/database_func.py
+++ backend/database_func.py
@@ -6,7 +6,7 @@ TABLES = {}
 TABLES['files'] = (
     "CREATE TABLE `files` ("
     "   `file_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "   `file_name` varchar(255) NOT NULL,"
+    "   `file_name` varchar(255) NOT NULL UNIQUE,"
     "   `version` int(11) NOT NULL,"
     "   PRIMARY KEY (`file_id`))"
 )
@@ -15,39 +15,29 @@ TABLES['files_version'] = (
     "   `file_version_id` int(11) NOT NULL AUTO_INCREMENT,"
     "   `file_id` int(11) NOT NULL,"
     "   `file_version` int(11) NOT NULL,"
-    "   `function` varchar(255) NOT NULL,"
+    "   `pandas_function` varchar(255) NOT NULL,"
     "   PRIMARY KEY (`file_version_id`),"
     "   FOREIGN KEY (`file_id`) REFERENCES files (`file_id`))"
 )
 
-insert_file = ("INSERT INTO files "
-              "(file_name, version) "
-              "VALUES ('test', 1)")
+query_insert_file =         ("INSERT INTO files (file_name, version) VALUES (%s, %s)")
 
-insert_file_version = ("INSERT INTO files_version"
-                      "(file_id, file_version, function)"  
-                      "VALUES (SELECT(file_id FROM files WHERE file_name = %s), %s, %s)")
+query_insert_file_version = ("INSERT INTO files_version (file_id, file_version, pandas_function) VALUES (%s, %s, %s)")
 
 def insert_file(file_name):
-    print('file_name' + file_name)
     mydb = mysql.connector.connect(
         host="mysqldb",
         user="root",
         password="p@ssw0rd1",
         database="PANDAS_UI"
     )
-    file_data = ("test_file", 1)
-    file_version_data = ("test_file", 1, 'INIT')
     cursor = mydb.cursor()
-    cursor.execute("INSERT INTO files "
-              "(file_name, version) "
-              "VALUES ('test', 1)")
-    # cursor.execute(insert_file, file_data)
-    # cursor.execute(insert_file_version, file_version_data)
+    cursor.execute(query_insert_file, (file_name, 1))
+    cursor.execute(query_insert_file_version, (cursor.lastrowid, 1, 'INIT'))
     mydb.commit()
     cursor.close()
     mydb.close()
-    return ''   
+    return 'inserted'   
 
 def init_tables():
     mydb = mysql.connector.connect(
