3b9ff401cbb33c89f307cc389d5de51b39128a81
stanz7 <stanleyzeng1998@gmail.com>
2022-04-07 01:30:17 -0400

    added working endpoints for rm na and rm dups

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,33 @@
-from flask import Flask, request, abort, send_from_directory, session
+from flask import Flask, request, abort, send_from_directory
 import os
 import pandas_func
-import uuid
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.csv']
-app.config['UPLOAD_PATH'] = '../csv/'
+app.config['UPLOAD_PATH'] = '../csv'
 app.config['MAX_HEADER_ROWS'] = 100
-app.secret_key = 'TODO_SECRET_KEY'
 
-USER_UUID_STR = 'user_uuid';
+@app.route('/hello')
+def load():
+    return '\n hello'
 
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
-    if USER_UUID_STR not in session:
-        session[USER_UUID_STR] = str(uuid.uuid4())
-
     uploaded_file = request.files['file']
-    file_name = uploaded_file.filename
-    if file_name != '':
-        file_ext = os.path.splitext(file_name)[1]
+    filename = uploaded_file.filename
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            return "Not a csv", 400
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], prefix_file_with_uuid(session, file_name)))
-        return "Saved", 200
-    return "No file attached", 400
+            return "\n Not a csv", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        return "\n Saved", 200
+    return "\n No file attached", 400
 
 @app.route("/downloader/<file_name>")
-def download_file(file_name):
-    if USER_UUID_STR not in sessions:
-        return 'UUID not set', 400
+def get_image(file_name):
     try:
-        return send_from_directory(directory=app.config['UPLOAD_PATH'], path=prefix_file_with_uuid(session, file_name))
+        return send_from_directory(directory=app.config["UPLOAD_PATH"], path=file_name)
     except FileNotFoundError:
         abort(404)        
         
@@ -44,22 +39,48 @@ def get_head(file_name, n):
 def get_tail(file_name, n):
     return head_or_tail(file_name, 'tail', n)
 
-def head_or_tail(file_name, direction, n):
-    if n > app.config['MAX_HEADER_ROWS']:
-        return 'Exceeds max configured records' , 404
-    try:
-        json = pandas_func.view_dataframe(app.config['UPLOAD_PATH'], file_name, direction, n)
+@app.route('/pandas/rm/dups/<string:file_name>/<string:column_name>')
+def remove_duplicates(file_name, column_name):
+    try: 
+        json = pandas_func.remove_duplicates(app.config['UPLOAD_PATH'], file_name, column_name)
         return json, 200
+    except KeyError:
+        return "\n Invalid column name.", 404
     except FileNotFoundError:
-        return 'File not found ' + file_name, 404     
+        return '\n File not found ' + file_name, 404 
+
+@app.route('/pandas/rm/na/<string:file_name>/<string:column_name>')
+def remove_na(file_name, column_name):
+    try: 
+        json = pandas_func.remove_na(app.config['UPLOAD_PATH'], file_name, column_name)
+        return json, 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404 
+
+
+#TODO
+@app.route('/pandas/rm/na_stat/<string:file_name>/<string:column_name>/<string:stat>')
+def remove_na_stat(file_name, column_name, stat):
+    try:
+        json = pandas_func.remove_na_stat(app.config['UPLOAD_PATH'], file_name, column_name, stat)
+        return json, 200
+    except KeyError:
+        return "\n Invalid column name.", 404
 
 @app.errorhandler(413)
 def too_large(e):
-    return "File is too large", 413
-
-def prefix_file_with_uuid(session, file_name):
-    return session[USER_UUID_STR] + '_' + file_name
+    return "\nFile is too large", 413
 
 if __name__ == "__main__":
     app.run()         
 
+def head_or_tail(file_name, direction, n):
+    if n > app.config['MAX_HEADER_ROWS']:
+        return '\n Exceeds max configured records' , 404
+    try:
+        json = pandas_func.view_dataframe(app.config['UPLOAD_PATH'], file_name, direction, n)
+        return '\n' + json, 200
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404     
