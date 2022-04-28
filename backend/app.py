722fbb6432704f5f25a5f901c058caf0585426e3
he-eric <eriche864@gmail.com>
2022-04-27 20:34:22 -0400

    refactored to rely on redis

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, abort, send_from_directory
+from io import StringIO
+from werkzeug.wrappers import Response
 from flask_cors import CORS
 import os
 import pandas_func
@@ -31,21 +33,23 @@ def load():
 def upload_file():
     uploaded_file = request.files['file']
     filename = uploaded_file.filename
+    file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             return "\n Not a csv", 400
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        datastore.store_df(filename, pd.read_csv(os.path.join(app.config['UPLOAD_PATH'], filename)))
+        uploaded_file.save(file_path)
+        datastore.store_df(filename, pd.read_csv(file_path))
+        os.remove(file_path)
         return "\n Saved", 200
     return "\n No file attached", 400
 
+#todo 
 @app.route("/downloader/<file_name>")
 def get_file(file_name):
-    try:
-        return send_from_directory(directory=app.config["UPLOAD_PATH"], path=file_name)
-    except FileNotFoundError:
-        abort(404)        
+    df = datastore.get_df(file_name)
+    csv_string = df.to_csv()
+    return {'data': csv_string}
         
 @app.route('/pandas/head/<string:file_name>/<int:n>')
 def get_head(file_name, n):
@@ -58,8 +62,8 @@ def get_tail(file_name, n):
 @app.route('/pandas/rm_dups/<string:file_name>/<string:column_name>')
 def remove_duplicates(file_name, column_name):
     try: 
-        json = pandas_func.remove_duplicates(app.config['UPLOAD_PATH'], file_name, column_name)
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.remove_duplicates(df, column_name)
         datastore.lpush(file_name, 'remove_duplicates:' + column_name)
         return json, 200
     except KeyError:
@@ -70,7 +74,8 @@ def remove_duplicates(file_name, column_name):
 @app.route('/pandas/get_dummies/<string:file_name>/<string:column_name>')
 def get_dummies(file_name, column_name):
     try:
-        json = pandas_func.getDummies(app.config['UPLOAD_PATH'], file_name, column_name)
+        df = datastore.get_df(file_name)
+        json = pandas_func.getDummies(df, file_name, column_name)
         return json, 200
     except KeyError:
         return "\n Invalid column name.", 404
@@ -80,8 +85,8 @@ def get_dummies(file_name, column_name):
 @app.route('/pandas/rm_nulls/<string:file_name>/<string:column_name>')
 def remove_nulls(file_name, column_name):
     try: 
-        json = pandas_func.remove_nulls(app.config['UPLOAD_PATH'], file_name, column_name)
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.remove_nulls(df, column_name)
         datastore.lpush(file_name, 'remove_nulls:' + column_name)
         return json, 200
     except KeyError:
@@ -92,8 +97,8 @@ def remove_nulls(file_name, column_name):
 @app.route('/pandas/replace/na_mean/<string:file_name>/<string:column_name>')
 def replace_na_mean(file_name, column_name):
     try:
-        json = pandas_func.replace_na_numeric(app.config['UPLOAD_PATH'], file_name, column_name, 'mean')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_numeric(df, column_name, 'mean')
         datastore.lpush(file_name, 'replace_na_man:' + column_name)
         return json, 200
     except KeyError:
@@ -104,8 +109,8 @@ def replace_na_mean(file_name, column_name):
 @app.route('/pandas/replace/na_median/<string:file_name>/<string:column_name>')
 def replace_na_median(file_name, column_name):
     try:
-        json = pandas_func.replace_na_numeric(app.config['UPLOAD_PATH'], file_name, column_name, 'median')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_numeric(df, column_name, 'median')
         datastore.lpush(file_name, 'replace_na_median:' + column_name)
         return json, 200
     except KeyError:
@@ -116,8 +121,8 @@ def replace_na_median(file_name, column_name):
 @app.route('/pandas/replace/na_mode_numeric/<string:file_name>/<string:column_name>')
 def replace_na_mode_numeric(file_name, column_name):
     try:
-        json = pandas_func.replace_na_numeric(app.config['UPLOAD_PATH'], file_name, column_name, 'mode')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_numeric(df, column_name, 'mode')
         datastore.lpush(file_name, 'replace_na_mode_numeric:' + column_name)
         return json, 200
     except KeyError:
@@ -128,8 +133,8 @@ def replace_na_mode_numeric(file_name, column_name):
 @app.route('/pandas/replace/na_unknown/<string:file_name>/<string:column_name>')
 def replace_na_unknown(file_name, column_name):
     try:
-        json = pandas_func.replace_na_categorical(app.config['UPLOAD_PATH'], file_name, column_name, 'unknown')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_categorical(df, column_name, 'unknown')
         datastore.lpush(file_name, 'remove_na_unknown:' + column_name)
         return json, 200
     except KeyError:
@@ -140,8 +145,8 @@ def replace_na_unknown(file_name, column_name):
 @app.route('/pandas/replace/na_ffill/<string:file_name>/<string:column_name>')
 def replace_na_ffill(file_name, column_name):
     try:
-        json = pandas_func.replace_na_categorical(app.config['UPLOAD_PATH'], file_name, column_name, 'ffill')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_categorical(df, column_name, 'ffill')
         datastore.lpush(file_name, 'replace_na_ffill:' + column_name)
         return json, 200
     except KeyError:
@@ -152,8 +157,8 @@ def replace_na_ffill(file_name, column_name):
 @app.route('/pandas/replace/na_bfill/<string:file_name>/<string:column_name>')
 def replace_na_bfill(file_name, column_name):
     try:
-        json = pandas_func.replace_na_categorical(app.config['UPLOAD_PATH'], file_name, column_name, 'bfill')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_categorical(df, column_name, 'bfill')
         datastore.lpush(file_name, 'replace_na_bfill:' + column_name)
         return json, 200
     except KeyError:
@@ -164,8 +169,8 @@ def replace_na_bfill(file_name, column_name):
 @app.route('/pandas/replace/na_mode_categorical/<string:file_name>/<string:column_name>')
 def replace_na_mode_categorical(file_name, column_name):
     try:
-        json = pandas_func.replace_na_categorical(app.config['UPLOAD_PATH'], file_name, column_name, 'mode')
-        pandas_func.export(json)
+        df = datastore.get_df(file_name)
+        json = pandas_func.replace_na_categorical(df, column_name, 'mode')
         datastore.lpush(file_name, 'replace_na_mode_categorical:' + column_name)
         return json, 200
     except KeyError:
@@ -176,7 +181,8 @@ def replace_na_mode_categorical(file_name, column_name):
 @app.route('/pandas/rename_column/<string:file_name>/<string:old_column>/<string:new_column>')
 def rename_column(file_name, old_column, new_column):
     try:
-        json = pandas_func.rename(app.config['UPLOAD_PATH'], file_name, old_column, new_column)
+        df = datastore.get_df(file_name)
+        json = pandas_func.rename(df, old_column, new_column)
         datastore.lpush(file_name, 'rename_column:' + old_column + ':' + new_column)
         return json, 200
     except KeyError:
@@ -187,7 +193,8 @@ def rename_column(file_name, old_column, new_column):
 @app.route('/pandas/normalize/<string:file_name>/<string:column_name>')
 def normalize(file_name, column_name):
     try:
-        json = pandas_func.normalize(app.config['UPLOAD_PATH'], file_name, column_name)
+        df = datastore.get_df(file_name)
+        json = pandas_func.normalize(df, column_name)
         datastore.lpush(file_name, 'normalize:' + column_name)
         return json, 200
     except KeyError:
--- backend/pandas_func.py
+++ backend/pandas_func.py
@@ -16,7 +16,7 @@ def export(jsonString):
     df.to_csv("../csv/exported.csv")
 
 # Transform categorical variable column into mulitple binary predictors
-def getDummies(path, file_name,col_name):
+def getDummies(df, col_name):
     df = pd.read_csv(os.path.join(path, file_name))
     if df[col_name].nunique() < 45:
         df1 = pd.get_dummies(df[col_name])
@@ -27,8 +27,7 @@ def getDummies(path, file_name,col_name):
         return "Error - too many variables"
 
 # """Remove duplicates given a column index, method is optional for keeping first or last"""
-def remove_duplicates(path, file_name, col_name, method="first"):
-    df = pd.read_csv(os.path.join(path, file_name))
+def remove_duplicates(df, col_name, method="first"):
     if df[col_name].duplicated().values.any():
         df = df.drop_duplicates(subset=col_name, keep=method)
         return json.dumps(json.loads(df.to_json(orient='records')))
@@ -36,8 +35,7 @@ def remove_duplicates(path, file_name, col_name, method="first"):
         return "No Duplicates"
 
 # """Remove nulls given a column index"""
-def remove_nulls(path, file_name, col_name):
-    df = pd.read_csv(os.path.join(path, file_name))
+def remove_nulls(df, col_name):
     if df[col_name].isna().values.any():
         df = df.dropna(subset=col_name)
         return json.dumps(json.loads(df.to_json(orient='records')))
@@ -45,14 +43,12 @@ def remove_nulls(path, file_name, col_name):
         return "No Nulls"
 
 # """Rename column name into new column name"""
-def rename(path, file_name, old_name, new_name):
-    df = pd.read_csv(os.path.join(path, file_name))
+def rename(df, old_name, new_name):
     df.rename(columns = {old_name:new_name}, inplace = True)
     return json.dumps(json.loads(df.to_json(orient='records')))
 
 # """This function should only be used for numeric columns. Replace Na stat with mean median or mode can later add regressions and fancy stuff as well to here. Probably call another function"""
-def replace_na_numeric(path, file_name, col_name, stat):
-    df = pd.read_csv(os.path.join(path, file_name))
+def replace_na_numeric(df, col_name, stat):
     if stat == 'mean':
         df[col_name].fillna(value=df[col_name].mean(), inplace=True)
     if stat == 'mode':
@@ -62,8 +58,7 @@ def replace_na_numeric(path, file_name, col_name, stat):
     return json.dumps(json.loads(df.to_json(orient='records')))
 
 # """This function should be used to treat nulls in categorical columns""""
-def replace_na_categorical(path, file_name, col_name, stat):
-    df = pd.read_csv(os.path.join(path, file_name))
+def replace_na_categorical(df, col_name, stat):
     if stat == "unknown":
         df[col_name].fillna("unknown", inplace = True)
     if stat == 'ffill':
@@ -75,8 +70,7 @@ def replace_na_categorical(path, file_name, col_name, stat):
     return json.dumps(json.loads(df.to_json(orient='records')))
 
 # """"Normalize Column"""
-def normalize_column(path, file_name, col_name):
-    df = pd.read_csv(os.path.join(path, file_name))
+def normalize_column(df, col_name):
     df_max_scaled = df.copy()
     df_max_scaled[col_name] = df_max_scaled[col_name] /df_max_scaled[col_name].abs().max()
     return json.dumps(json.loads(df_max_scaled.to_json(orient='records')))
