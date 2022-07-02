6efcc0c362f5ef89fed3580c90c5c3e7267048e9
stanz7 <stanleyzeng1998@gmail.com>
2022-07-02 16:01:14 -0400

    permutations for date_transformer added

--- backend/app.py
+++ backend/app.py
@@ -319,12 +319,72 @@ def change_data_type_to_date(file_name, column_name):
     except FileNotFoundError:
         return '\n File not found ' + file_name, 404
 
-@app.route('/pandas/date_transformer/<string:file_name/<string:column_name/<string:type>')
-def date_transformer(file_name, column_name, type):
+@app.route('/pandas/date_transformer_day/<string:file_name/<string:column_name')
+def date_transformer_day(file_name, column_name):
     try: 
         df = datastore.get_df(file_name);
-        json = pandas_func.dateTransformer(df, column_name, type)
-        datastore.lpush(file_name, 'date_transformer:' + column_name + ':' + type, df)
+        json = pandas_func.dateTransformer(df, column_name, "day")
+        datastore.lpush(file_name, 'date_transformer_day:' + column_name, df)
+        return df_to_json(df), 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404
+
+@app.route('/pandas/date_transformer_month/<string:file_name/<string:column_name')
+def date_transformer_month(file_name, column_name):
+    try: 
+        df = datastore.get_df(file_name);
+        json = pandas_func.dateTransformer(df, column_name, "month")
+        datastore.lpush(file_name, 'date_transformer_month:' + column_name, df)
+        return df_to_json(df), 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404
+
+@app.route('/pandas/date_transformer_year/<string:file_name/<string:column_name')
+def date_transformer_year(file_name, column_name):
+    try: 
+        df = datastore.get_df(file_name);
+        json = pandas_func.dateTransformer(df, column_name, "year")
+        datastore.lpush(file_name, 'date_transformer_year:' + column_name, df)
+        return df_to_json(df), 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404
+
+@app.route('/pandas/date_transformer_quarter/<string:file_name/<string:column_name')
+def date_transformer_quarter(file_name, column_name):
+    try: 
+        df = datastore.get_df(file_name);
+        json = pandas_func.dateTransformer(df, column_name, "quarter")
+        datastore.lpush(file_name, 'date_transformer_quarter:' + column_name, df)
+        return df_to_json(df), 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404
+
+@app.route('/pandas/date_transformer_day_of_week/<string:file_name/<string:column_name')
+def date_transformer_day_of_week(file_name, column_name):
+    try: 
+        df = datastore.get_df(file_name);
+        json = pandas_func.dateTransformer(df, column_name, "dayofweek")
+        datastore.lpush(file_name, 'date_transformer_day_of_week:' + column_name, df)
+        return df_to_json(df), 200
+    except KeyError:
+        return "\n Invalid column name.", 404
+    except FileNotFoundError:
+        return '\n File not found ' + file_name, 404
+
+@app.route('/pandas/date_transformer_weekday/<string:file_name/<string:column_name')
+def date_transformer_weekday(file_name, column_name):
+    try: 
+        df = datastore.get_df(file_name);
+        json = pandas_func.dateTransformer(df, column_name, "weekday")
+        datastore.lpush(file_name, 'date_transformer_weekday:' + column_name, df)
         return df_to_json(df), 200
     except KeyError:
         return "\n Invalid column name.", 404
